301fecbcb2f7fc99beb6dac821608904fb1922ff
Antonio Luiz Neto <antonioluiz.n.eng@gmail.com>
2024-03-04 23:10:38 -0300

    py

---
+++ First.py
@@ -0,0 +1,14 @@
+name = input("Insira seu nome: ")
+print(name)
+altura = int(input("Insira a altura do triangulo: "))
+base = int(input("insira a base do triangulo: "))
+area = (base * altura)/2
+print("A area do triangulo é ", area)
+
+raio = float(input("Insira o raio do circulo: "))
+area_c = 3.14*(raio**2)
+print("A area do circulo é ", area_c)
+
+nome = input("Insira o nome: ")
+idade = int(input("Insira sua idade: "))
+print("O", nome, "possui", idade, "anos")
