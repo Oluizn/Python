a96af0ee7dd4e7a6d234aa00ab2a347dae538e57
Antonio Luiz Neto <antonioluiz.n.eng@gmail.com>
2024-03-26 23:09:12 -0300

    Python estatistica Exercicio resolvido

--- Exercicio51.py
+++ Exercicio51.py
@@ -6,7 +6,7 @@ def probabilidade_rei(ciclo, rei):
     cartas_no_baralho = 52  # Total de cartas no baralho
     cartas_por_naipe = 4  # Cartas iguais com naipe diferente (por exemplo, 4 reis)
     for i in range(ciclo):
-        resultado = random.randint(1, cartas_no_baralho // cartas_por_naipe)  # Simulando retirada de cartas, como são 52 cartas e cada carta possui 4 variações, totaliza 13 de espaço amostral
+        resultado = random.randint(1, cartas_no_baralho // cartas_por_naipe)    # Simulando retirada de cartas, como são 52 cartas e cada carta possui 4 variações, totaliza 13 de espaço amostral
         if resultado == rei:
             vezes_numero_aparece += 1
     return vezes_numero_aparece / ciclo
@@ -114,8 +114,8 @@ probabilidade_em_sequencia2 = probabilidade_rei_sequencia2(ciclo, carta_rei)
 probabilidade_em_sequencia4 = probabilidade_rei_sequencia4(ciclo, carta_rei)
 probabilidade_em_sequencia2_unreplaced = probabilidade_rei_sequencia2_unreplaced(ciclo, lista_rei)
 probabilidade_em_sequencia4_unreplaced = probabilidade_rei_sequencia4_unreplaced(ciclo, lista_rei)
-print(f"A probabilidade de sair a carta rei em {ciclo} ciclos é de aproximadamente {probabilidade*100:.10f}%")
-print(f"A probabilidade de sair a carta rei duas vezes seguidas, com a carta rei sendo reposta no baralho, em {ciclo} ciclos é de aproximadamente {probabilidade_em_sequencia2*100:.10f}%")
-print(f"A probabilidade de sair a carta rei quatro vezes seguidas, com a carta rei sendo reposta no baralho, em {ciclo} ciclos é de aproximadamente {probabilidade_em_sequencia4*100:.10f}%")
-print(f"A probabilidade de sair a carta rei 2 vezes seguidas, com a carta rei nao sendo reposta no baralho, em {ciclo} ciclos é de aproximadamente {probabilidade_em_sequencia2_unreplaced*100:.10f}%")
-print(f"A probabilidade de sair a carta rei 4 vezes seguidas, com a carta rei nao sendo reposta no baralho, em {ciclo} ciclos é de aproximadamente {probabilidade_em_sequencia4_unreplaced*100:.10f}%")
+print(f"A probabilidade de sair a carta rei é de aproximadamente {probabilidade*100:.2f}%")
+print(f"A probabilidade de sair a carta rei duas vezes seguidas, com a carta rei sendo reposta no baralho, é de aproximadamente {probabilidade_em_sequencia2*100:.2f}%")
+print(f"A probabilidade de sair a carta rei quatro vezes seguidas, com a carta rei sendo reposta no baralho, é de aproximadamente {probabilidade_em_sequencia4*100:.4f}%")
+print(f"A probabilidade de sair a carta rei 2 vezes seguidas, com a carta rei nao sendo reposta no baralho, é de aproximadamente {probabilidade_em_sequencia2_unreplaced*100:.2f}%")
+print(f"A probabilidade de sair a carta rei 4 vezes seguidas, com a carta rei nao sendo reposta no baralho, é de aproximadamente {probabilidade_em_sequencia4_unreplaced*100:.4f}%")
